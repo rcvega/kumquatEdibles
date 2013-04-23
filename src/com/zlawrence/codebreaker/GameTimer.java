e7aa2b12504541e764712b53dc385ced5541d686
Zachary Lawrence <zdlawrence@gmail.com>
2013-04-22 20:47:40 -0600

    Finished commenting the code.

--- src/com/zlawrence/codebreaker/GameTimer.java
+++ src/com/zlawrence/codebreaker/GameTimer.java
@@ -3,21 +3,37 @@ package com.zlawrence.codebreaker;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Class for putting a time constraint on the game.
+ * @author Zachary Lawrence
+ * @since 4/22/2013
+ */
 public class GameTimer extends TimerTask {
 	
-	Game currentGame;
-	long timerDuration;
+	Game currentGame; //the current game in play
+	long timerDuration; //the duration of the timer in milliseconds
 	
+	/**
+	 * Constructor for GameTimer.
+	 * @param currentGame The game in play.
+	 * @param timerDuration The duration of the timer to create in seconds.
+	 */
 	GameTimer(Game currentGame, long timerDuration) {
 		this.currentGame = currentGame;
-		this.timerDuration = 1000*timerDuration;
+		this.timerDuration = 1000*timerDuration; //convert to seconds
 	}
 	
+	/**
+	 * Method that "starts" the timer.
+	 */
 	public void set() {
 		Timer timer = new Timer();
 		timer.schedule(this, timerDuration);
 	}
 	
+	/**
+	 * When the timer is up, this method is executed.
+	 */
 	public void run() {
 	    currentGame.timeExpired();
 	}
